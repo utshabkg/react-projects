b72df37b2d303dafd5aa08421df8541f1f5f1892
utshabkg <ranjanyouth@gmail.com>
2023-03-16 12:08:41 +0600

    added homepage as Route in Cards.

--- movie-review/src/App.js
+++ movie-review/src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Cards from "./components/Cards";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Cards />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+      </Routes>
     </div>
   );
 }
