308416bbd8c8079928c59f83202c4e7cc74cbf20
utshabkg <ranjanyouth@gmail.com>
2023-03-16 15:25:07 +0600

    data from firebase is showing in page.

--- movie-review/src/components/Cards.js
+++ movie-review/src/components/Cards.js
@@ -1,92 +1,58 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Vortex } from "react-loader-spinner";
 import ReactStars from "react-stars";
+import { getDocs } from "firebase/firestore";
+import { moviesRef } from "./firebase/firebase";
 
 const Cards = () => {
-  const [data, setData] = useState([
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 4.5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 3.5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-    {
-      name: "Avengers Endgame",
-      year: 2020,
-      rating: 5,
-      img: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
-    },
-  ]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const _data = await getDocs(moviesRef);
+      _data.forEach((doc) => {
+        setData((prev) => [...prev, doc.data()]);
+      });
+      setLoading(false);
+    }
+    getData();
+  }, []);
+
   return (
     <div className="flex flex-wrap justify-between p-3 mt-2">
-      {data.map((e, i) => {
-        return (
-          <div
-            key={i}
-            className="card font-medium shadow-lg p-1 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500"
-          >
-            <img className="h-72" src={e.img} />
-            <h1>
-              <span className="text-gray-400">Movie:</span> {e.name}
-            </h1>
-            <h1 className="flex items-center">
-              <span className="text-gray-400 mr-1">Rating:</span>
-              <ReactStars size={20} half={true} value={e.rating} edit={false} />
-            </h1>
-            <h1>
-              <span className="text-gray-400">Year:</span> {e.year}
-            </h1>
-          </div>
-        );
-      })}
+      {loading ? (
+        <div className="w-full flex justify-center items-center h-96">
+          <Vortex height={40} color="white" />
+        </div>
+      ) : (
+        data.map((e, i) => {
+          return (
+            <div
+              key={i}
+              className="card font-medium shadow-lg p-1 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500"
+            >
+              <img className="h-72" src={e.image} />
+              <h1>
+                <span className="text-gray-400">Movie:</span> {e.title}
+              </h1>
+              <h1 className="flex items-center">
+                <span className="text-gray-400 mr-1">Rating:</span>
+                <ReactStars
+                  size={20}
+                  half={true}
+                  value={e.rating}
+                  edit={false}
+                />
+              </h1>
+              <h1>
+                <span className="text-gray-400">Year:</span> {e.year}
+              </h1>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
