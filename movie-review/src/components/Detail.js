c3b3921adc10a9831a0511e1390c93d7d003539e
utshabkg <ranjanyouth@gmail.com>
2023-03-17 12:23:51 +0600

    Review Page recreated.

--- movie-review/src/components/Detail.js
+++ movie-review/src/components/Detail.js
@@ -42,18 +42,8 @@ const Detail = () => {
             <h1 className="text-3xl font-bold text-gray-400">
               {data.title} <span className="text-xl">({data.year})</span>
             </h1>
-            <ReactStars
-              size={20}
-              half={true}
-              value={data.rating / data.number_of_ratings}
-              edit={false}
-            />
             <p className="mt-3">{data.description}</p>
-            <Reviews
-              id={id}
-              prevRating={data.rating}
-              number_of_ratings={data.number_of_ratings}
-            />
+            <ReactStars size={20} half={true} value={4.5} edit={false} />
           </div>
         </>
       )}
